4d2e2b51666a3ec0342024969d5271faef4ae078
Silaswalter <129095844+Silaswalter@users.noreply.github.com>
2023-03-29 23:56:40 -0500

    Added bool to exits

---
+++ DungeonCrawlerCSC250/Assets/Scripts/DungeonSetup.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonSetup : MonoBehaviour
+{
+    public GameObject northExit;
+    public GameObject southExit;
+    public GameObject eastExit;
+    public GameObject westExit;
+    
+    public bool northOn, southOn, eastOn, westOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        northOn = true;
+        southOn = false;
+        eastOn = true;
+        westOn = false;
+
+        
+        if(northOn != true)
+        {
+            this.northExit.SetActive(false);
+        }
+
+        if(southOn != true)
+        {
+            this.southExit.SetActive(false);
+        }
+
+        if(eastOn != true)
+        {
+            this.eastExit.SetActive(false);
+        }
+
+        if(westOn != true)
+        {
+            this.westExit.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
